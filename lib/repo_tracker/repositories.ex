3318891de8283aa097615d85b0e367e8cfdc043a
André Luiz <andre@nash.io>
2022-10-15 18:49:01 -0300

    reming unnecessary module

--- lib/repo_tracker/repositories.ex
+++
@@ -1,7 +0,0 @@
-defmodule RepoTracker.Repositories do
-  @moduledoc """
-  Module reponsible to provide an api to work with repositories including cross-context functionalities
-  """
-
-  alias RepoTracker.Providers
-end
