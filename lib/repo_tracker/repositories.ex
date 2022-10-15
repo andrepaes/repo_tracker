13e4a6583f7af25a1715bde5844bec26b7dde4ba
André Luiz <andre@nash.io>
2022-10-15 18:43:57 -0300

    adding repo_info to providers

--- lib/repo_tracker/repositories.ex
+++ lib/repo_tracker/repositories.ex
@@ -4,19 +4,4 @@ defmodule RepoTracker.Repositories do
   """
 
   alias RepoTracker.Providers
-
-  @spec fetch_issues(Providers.provider(), Providers.repo(), Providers.login()) ::
-          {:ok, [{title :: String.t(), author :: String.t(), labels :: [String.t()]}]}
-          | Providers.error()
-  def fetch_issues(provider, repo_name, login) do
-    with {:ok, issues} <- Providers.list_issues(provider, login, repo_name) do
-      formatted_issue =
-        Enum.map(issues, fn
-          %{title: title, login: author, labels: labels} ->
-            {title, author, labels}
-        end)
-
-      {:ok, formatted_issue}
-    end
-  end
 end
