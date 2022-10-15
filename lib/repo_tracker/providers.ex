a6beadc54eb531702ecd71e0e7f9825f16a6d66d
André Luiz <andre@nash.io>
2022-10-15 18:43:57 -0300

    adding repo_info to providers

--- lib/repo_tracker/providers.ex
+++ lib/repo_tracker/providers.ex
@@ -8,10 +8,24 @@ defmodule RepoTracker.Providers do
   @type login :: String.t()
   @type provider :: :github
   @type error :: %{message: String.t(), code: non_neg_integer()}
+  @type repo_info :: %{
+          required(:issues) => [IssueResponse.t()],
+          required(:contributors) => [ContributorResponse.t()]
+        }
 
   alias RepoTracker.Providers.ContributorResponse
   alias RepoTracker.Providers.IssueResponse
+  alias RepoTracker.Providers.UserResponse
   alias RepoTracker.Providers.GithubImpl
+
+  @spec fetch_repo_info(Providers.provider(), Providers.login(), Providers.repo()) ::
+          {:ok, repo_info()} | {:error, error()}
+  def fetch_repo_info(provider, login, repo_name) do
+    with {:ok, issues} <- list_issues(provider, login, repo_name),
+         {:ok, contributors} <- list_contributors(provider, login, repo_name) do
+      {:ok, %{issues: issues, contributors: contributors}}
+    end
+  end
 
   @spec list_issues(provider(), owner(), repo()) :: {:ok, [IssueResponse.t()]} | {:error, error()}
   def list_issues(provider, owner, repo) do
--- lib/repo_tracker/providers/contributor_response.ex
+++ lib/repo_tracker/providers/contributor_response.ex
@@ -1,9 +1,8 @@
 defmodule RepoTracker.Providers.ContributorResponse do
   @type t :: %{
-          name: String.t(),
           login: String.t(),
           commits_quantity: non_neg_integer()
         }
 
-  defstruct [:login, :commits_quantity, :name]
+  defstruct [:login, :commits_quantity]
 end
