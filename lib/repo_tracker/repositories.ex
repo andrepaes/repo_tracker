de77ccf72339bc6b21dfe72bd37d748de692b934
André Luiz <andre@nash.io>
2022-10-15 18:51:21 -0300

    adding repositories

---
+++ lib/repo_tracker/repositories.ex
@@ -0,0 +1,8 @@
+defmodule RepoTracker.Repositories do
+  @moduledoc """
+  Module reponsible to provide an api to work with repositories including cross-context functionalities
+  """
+
+  alias RepoTracker.Providers
+
+end
